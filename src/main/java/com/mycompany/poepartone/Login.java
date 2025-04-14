4576e596d047715ada39e31c999841665f02ebf1
Rafael Pilartes <81381729+RafaelPilartes@users.noreply.github.com>
2025-04-14 13:53:53 +0200

    Structuring the project

---
+++ src/main/java/com/mycompany/poepartone/Login.java
@@ -0,0 +1,4 @@
+package com.mycompany.poepartone;
+
+public class Login {
+}
---
+++ src/main/java/com/mycompany/poepartone/POEPartOne.java
@@ -0,0 +1,8 @@
+package com.mycompany.poepartone;
+
+public class POEPartOne {
+
+    public static void main(String[] args) {
+        
+    }
+}
